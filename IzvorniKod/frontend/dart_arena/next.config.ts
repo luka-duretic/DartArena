9b1a3ff091d9e5a00d808170d557b35d5dc328b6
luka-duretic <luka.duretic@fer.unizg.hr>
2025-06-14 14:18:25 +0200

    deploy fix

--- IzvorniKod/frontend/dart_arena/next.config.ts
+++ IzvorniKod/frontend/dart_arena/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  //output: 'export',
+  assetPrefix: './',
 };
 
 export default nextConfig;
